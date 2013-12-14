4483bc1d032c5a8d8a3e9cc4cc88a4a364c3c2e1
gatgui <gatgui@gmail.com>
2013-12-15 00:02:54 +0900

    initial commit

---
+++ SConstruct
@@ -0,0 +1,17 @@
+import sys
+import glob
+import excons
+from excons.tools import arnold
+
+env = excons.MakeBaseEnv()
+
+prjs = [
+  {"name": "agUserDataRamp",
+   "type": "dynamicmodule",
+   "ext": arnold.PluginExt(),
+   "srcs": glob.glob("*.cpp"),
+   "custom": [arnold.Require]
+  }
+]
+
+excons.DeclareTargets(env, prjs)
---
+++ agUserDataColorRamp.cpp
@@ -0,0 +1,126 @@
+#include "agUserDataRamp.h"
+
+AI_SHADER_NODE_EXPORT_METHODS(agUserDataColorRampMtd);
+
+enum FloatRampParams
+{
+   p_input = 0,
+   p_positions,
+   p_values,
+   p_interpolations,
+   p_default_interpolation,
+   p_default_value,
+   p_abort_on_error
+};
+
+node_parameters
+{
+   AiParameterFlt("input", 0.0f);
+   AiParameterStr("positions", "");
+   AiParameterStr("values", "");
+   AiParameterStr("interpolations", "");
+   AiParameterEnum("default_interpolation", 1, InterpolationTypeNames);
+   AiParameterRGB("default_value", 1.0f, 0.0f, 1.0f);
+   AiParameterBool("abort_on_error", false);
+};
+
+node_initialize
+{
+}
+
+node_update
+{
+}
+
+node_finish
+{
+}
+
+static void Failed(AtShaderGlobals *sg, AtNode *node, const char *errMsg)
+{
+   if (AiShaderEvalParamBool(p_abort_on_error))
+   {
+      if (errMsg)
+      {
+         AiMsgError("[userDataColorRamp] %s", errMsg);
+      }
+      else
+      {
+         AiMsgError("[userDataColorRamp] Failed");
+      }
+   }
+   else
+   {
+      if (errMsg)
+      {
+         AiMsgWarning("[userDataColorRamp] %s. Use default value", errMsg);
+      }
+      else
+      {
+         AiMsgWarning("[userDataColorRamp] Failed. Use default value");
+      }
+      sg->out.RGB = AiShaderEvalParamRGB(p_default_value);
+   }
+}
+
+shader_evaluate
+{
+   const char *pname = AiShaderEvalParamStr(p_positions);
+   const char *vname = AiShaderEvalParamStr(p_values);
+   const char *iname = AiShaderEvalParamStr(p_interpolations);
+   
+   AtArray *p, *v, *i=0;
+   
+   if (!AiUDataGetArray(pname, &p))
+   {
+      Failed(sg, node, "Positions user attribute not found or not an array");
+      return;
+   }
+   
+   if (p->type != AI_TYPE_FLOAT)
+   {
+      Failed(sg, node, "Positions user attribute must be an array of floats");
+      return;
+   }
+   
+   if (!AiUDataGetArray(vname, &v))
+   {
+      Failed(sg, node, "Values user attribute not found or not an array");
+      return;
+   }
+   
+   if (v->type != AI_TYPE_RGB)
+   {
+      Failed(sg, node, "Values user attribute must be an array of colors");
+      return;
+   }
+   
+   if (v->nelements != p->nelements)
+   {
+      Failed(sg, node, "Positions and values array size mismatch");
+      return;
+   }
+   
+   if (AiUDataGetArray(iname, &i))
+   {
+      if (i->type != AI_TYPE_ENUM && i->type != AI_TYPE_INT)
+      {
+         Failed(sg, node, "Interpolations user attribute must be an array of enums or ints");
+         return;
+      }
+      
+      if (i->nelements != p->nelements)
+      {
+         Failed(sg, node, "Positions and interpolations array size mismatch");
+         return;
+      }
+   }
+   
+   InterpolationType defi = (InterpolationType) AiShaderEvalParamInt(p_default_interpolation);
+   
+   unsigned int *s = (unsigned int*) AiShaderGlobalsQuickAlloc(sg, p->nelements * sizeof(unsigned int));
+   
+   SortPositions(p, s);
+   
+   EvalColorRamp(p, v, i, defi, s, AiShaderEvalParamFlt(p_input), sg->out.RGB);
+}
